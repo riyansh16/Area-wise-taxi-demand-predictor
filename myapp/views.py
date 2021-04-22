38353ce5f620770a8c59095d5e594fc8b476bc4b
Riyansh Pal <riyansh2502@gmail.com>
2021-04-22 22:37:33 +0530

    update view and model

--- myapp/views.py
+++ myapp/views.py
@@ -3,20 +3,21 @@ import pickle
 import os
 from . models import Prediction
 
+# Create your views here.
+
 def index(request):
-    # res=Prediction.objects.all()
-    return render(request, "index.html")
+    res = Prediction.objects.all()
+    return render(request,"index.html",{'res':res})
 
 def test(request):
-    ppw=int(request.POST['ppw'])
-    pn=int(request.POST['pn'])
-    mi=int(request.POST['mi'])
-    appw=int(request.POST['appw'])
-    modulepath=os.path.dirname(__file__)
-    path=os.path.join(modulepath,'taxi.pkl')
-    model=pickle.load(open(path,'rb'))
-    res=str(model.predict([[ppw,pn,mi,appw]])[0].round(2))
-    pre=Prediction(ppw=str(ppw),pn=str(pn),mi=str(mi),appm=str(appw),result=res)
+    ppw = int(request.POST['ppw'])
+    pn = int(request.POST['pn'])
+    mi = int(request.POST['mi'])
+    appm = int(request.POST['appm'])
+    modulepath = os.path.dirname(__file__)
+    model = pickle.load(open(os.path.join(modulepath,'taxi.pkl'),'rb'))
+    res = str(model.predict([[ppw,pn,mi,appm]])[0].round(2))
+    # return render(request,"index.html",{'res':res})
+    pre = Prediction(ppw=str(ppw),pn=str(pn),mi=str(mi),appm=str(appm),result=res) #ORM
     pre.save()
-    return render(request,'index.html',{'res':res})
-# Create your views here.
+    return redirect('index')
